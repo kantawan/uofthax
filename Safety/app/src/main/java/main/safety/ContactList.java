cd1037c5f846c3f39e43ca08f1b27e03c99b55ec
Erwin-Jeffrey Komguem <erwinjeffrey.komguem@mail.utoronto.ca>
2018-01-20 12:29:15 -0500

    still a work in progress, need to test text messaging feature

--- Safety/app/src/main/java/main/safety/ContactList.java
+++ Safety/app/src/main/java/main/safety/ContactList.java
@@ -5,73 +5,60 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-
-public class ContactList extends AppCompatActivity {
-
-
+public class ContactList extends AppCompatActivity implements Serializable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
 
-
-
         Button toMain =  (Button) findViewById(R.id.back);
+        final EditText p1 = (EditText)findViewById(R.id.editText);
+        final EditText p2 = (EditText)findViewById(R.id.editText2);
+        final EditText p3 = (EditText)findViewById(R.id.editText3);
+
         toMain.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 Intent backIntent = new Intent(ContactList.this, Main.class);
                 startActivity(backIntent);
+
+                String[] array = {p1.getText().toString(), p2.getText().toString(), p3.getText().toString()};
+                saveContacts(array);
             }
         });
-
-
-        // Button countText = (Button) findViewById(R.id.start);
     }
 
+    static private ArrayList<String> contacts; // = new ArrayList<>();
+    // private String[] contacts = new String[3];
 
-//    //erwin
-//    private CountDownTimer timer;
-//    private long  millisLeft;
-//    private TextView countText;
-//    private int counter = 0;
-//    private EditText input;
-//    private SmsManager smsManager = SmsManager.getDefault();
-//
-//    private void trigger() {
-//        smsManager.sendTextMessage("+12893888447", null, input.getText().toString(), null, null);
-//        Toast.makeText(this, "Your emergency contacts have been contacted.", Toast.LENGTH_SHORT).show();
-//    }
-//
-//
-//    public void onStart(View view) {
-//        timer = new CountDownTimer(60 * 1000, 1000){
-//            @Override
-//            public void onTick(long millisLeft){
-//                updateTimer();
-//            }
-//            @Override
-//            public void onFinish(){
-//                // Call trigger() method!!
-//                trigger();
-//            }
-//        }.start();
-//    }
-//    // TODO: call onStart in body of onClick.
-//
-//    public void updateTimer(){
-//        int minutes = (int) millisLeft / 60000;
-//        int seconds = (int) millisLeft % 60000 / 1000;
-//        String timeLeft = "" + minutes + ":";
-//        if (seconds < 10) timeLeft += "0";
-//        timeLeft += seconds + "";
-//
-//        countText.setText(timeLeft);
-//    }
+    public void saveContacts(String[] a) {
 
+        // System.arraycopy( a, 0, contacts, 0, a.length );
+        contacts = new ArrayList<>(Arrays.asList(a));
+        try {
+            for (int i = 0; i <= 2; i++) {
+                if (contacts.get(i).length()==0) {
+                    contacts.remove(i);
+                }
+            }
+        }
+        finally {
+            int s = contacts.size();
+            System.out.println("Hello");
+        }
+    }
+
+    public static ArrayList<String> getContacts() {
+        return contacts;
+    }
 
 }
