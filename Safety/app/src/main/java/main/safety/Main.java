1d50a7405ac53ebf541c9c5b16b00e3fd2219e9f
Erwin-Jeffrey Komguem <erwinjeffrey.komguem@mail.utoronto.ca>
2018-01-20 13:36:51 -0500

    Still a work in progress, working on implementation of serializable.

--- Safety/app/src/main/java/main/safety/Main.java
+++ Safety/app/src/main/java/main/safety/Main.java
@@ -1,23 +1,40 @@
 package main.safety;
 
+import android.content.Context;
+import android.os.DropBoxManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.lang.String;
+
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 import android.os.CountDownTimer;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 
-public class Main extends AppCompatActivity {
+public class Main extends AppCompatActivity implements Serializable {
     private EditText countDownText;
     private Button countDownButton;
+    private SmsManager smsManager = SmsManager.getDefault();
 
     private CountDownTimer countDownTimer;
     private long timeLeft = 600000;
-
-
 
     private boolean flag;
 
@@ -28,7 +45,6 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         // contact button
         Button toContact =  findViewById(R.id.contact_list);
         toContact.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +53,30 @@ public class Main extends AppCompatActivity {
             public void onClick(View view) {
                 Intent contactIntent = new Intent(Main.this, ContactList.class);
                 startActivity(contactIntent);
+
+                // deserialize
+                try {
+                    FileInputStream fis = openFileInput("contacts.txt");
+                    ObjectInputStream is = new ObjectInputStream(fis);
+                    AppCompatActivity simpleClass = (AppCompatActivity) is.readObject();
+                    is.close();
+                    fis.close();
+                } catch (IOException e) {
+                    System.out.println("a turn up ting");
+                } catch (ClassNotFoundException e) {
+                    System.out.println("Boo hoo");
+                }
+
+//                JSONObject json = new JSONObject();
+//                try {
+//                    BufferedReader br = new BufferedReader(new FileReader("storage.json"));
+//                    DropBoxManager.Entry e = json.fromJson(br, DropBoxManager.Entry.class);
+//                    Log.d("reading", e.toString());
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
             }
+
         });
 
         //timer button
@@ -133,10 +172,25 @@ public class Main extends AppCompatActivity {
     public void trigger(){
 
         countDownText.setText("FUCK");
+        smsManager.sendTextMessage("+12893888447", null, "HELP!!", null, null);
+        Toast.makeText(this, "Your emergency contacts have been contacted.", Toast.LENGTH_SHORT).show();
 
 
 
     }
+
+//    public void close() {
+//        // serialize here
+//        try {
+//            FileOutputStream fos = context.openFileOutput("contacts.txt", Context.MODE_PRIVATE);
+//            ObjectOutputStream os = new ObjectOutputStream(fos);
+//            os.writeObject(this);
+//            os.close();
+//            fos.close();
+//        } catch (IOException e) {
+//            System.out.println("shit");
+//        }
+//    }
 
 
 
