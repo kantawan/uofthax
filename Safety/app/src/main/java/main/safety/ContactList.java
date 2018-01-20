26bb33c4df99c55472de14677d01ecef534c4507
Kantawan Charoenphiphob <c.kantawan@gmail.com>
2018-01-20 04:19:04 -0500

    contact to main, main to contact activity button

--- Safety/app/src/main/java/main/safety/ContactList.java
+++ Safety/app/src/main/java/main/safety/ContactList.java
@@ -1,7 +1,10 @@
 package main.safety;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactList extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class ContactList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+
+        Button toMain =  (Button) findViewById(R.id.back);
+        toMain.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent backIntent = new Intent(ContactList.this, Main.class);
+                startActivity(backIntent);
+            }
+        });
     }
 }
--- Safety/app/src/main/java/main/safety/Main.java
+++ Safety/app/src/main/java/main/safety/Main.java
@@ -2,6 +2,9 @@ package main.safety;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button toContact =  (Button) findViewById(R.id.contact_list);
+        toContact.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent contactIntent = new Intent(Main.this, ContactList.class);
+                startActivity(contactIntent);
+            }
+        });
     }
+
+
+
 }
