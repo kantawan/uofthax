a4ed6e7224650efecf001b8c8ce54472f17b2264
Angela-Zhu <angelahjzhu@gmail.com>
2018-01-20 23:23:22 -0500

    Add Location Updates
    Still not working (I am sad)

--- Safety/app/src/main/java/main/safety/Main.java
+++ Safety/app/src/main/java/main/safety/Main.java
@@ -1,7 +1,17 @@
 package main.safety;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.DropBoxManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,6 +25,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.lang.String;
+import java.util.List;
+import java.util.Locale;
 
 import android.util.Log;
 import android.widget.Button;
@@ -33,6 +45,11 @@ import org.json.JSONObject;
 public class Main extends AppCompatActivity {
     private EditText countDownText;
     private Button countDownButton;
+
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private String address;
 
 
     private CountDownTimer countDownTimer;
@@ -101,6 +118,59 @@ public class Main extends AppCompatActivity {
 
         });
 
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                try {
+                    address = getAddress(location.getLatitude(), location.getLongitude());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        // asks user for permission to access location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET
+            }, 10);
+            return;
+        }
+        else {
+            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+        }
+
+    }
+
+    // what to do when user agrees to give permission for location
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_DENIED)
+                    //location update (ignore the red under line it's nothing)
+                    locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+                return;
+        }
     }
 
     public void startStopTimer(){
@@ -172,6 +242,8 @@ public class Main extends AppCompatActivity {
     }
 
     public void trigger(){
+        //location update (ignore the red under line it's nothing)
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
 
         //String[] contacts = ContactList.getContacts();
         SharedPreferences data = getSharedPreferences(SHARE_PREF, MODE_PRIVATE);
@@ -186,7 +258,8 @@ public class Main extends AppCompatActivity {
             if (contacts[i] != null && contacts[i] != ""){
                 try {
                     SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage("+1" + contacts[i], null, "HELLOOOO!!", null, null);
+                    smsManager.sendTextMessage("+1" + contacts[i], null,
+                            "This is an automatic emergency message! \n I'm at " + address +".", null, null);
                     Toast.makeText(this, "Your emergency contacts have been contacted.", Toast.LENGTH_SHORT).show();
                 } catch (Exception e) {
                     System.out.println("damn");
@@ -195,6 +268,22 @@ public class Main extends AppCompatActivity {
             }
         }
 
+    }
+
+
+    public String getAddress(double latitude, double longitude) throws IOException {
+
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+        addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        String city = addresses.get(0).getLocality();
+        String state = addresses.get(0).getAdminArea();
+        String country = addresses.get(0).getCountryName();
+
+        return address + ", " + city + ", " + state + ", " + country;
     }
 
 }
